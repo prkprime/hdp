ffe5cd4baa2a39cb35799e09d75206fa8fe2fc4e
Pratik Gorade <pratikgorade0@gmail.com>
2021-06-05 12:35:57 +0530

    docker goes brrrr
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

---
+++ install-deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Disable Prompt for manual feedback
+export DEBIAN_FRONTEND=noninteractive
+
+pip3 install --no-cache-dir poetry
+poetry config virtualenvs.create false
+poetry install --no-dev --no-interaction --no-ansi
