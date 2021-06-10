653a1c84d292c687a8fa86823341d98658a1c1eb
Pratik Gorade <pratikgorade0@gmail.com>
2021-06-10 16:01:09 +0530

    clear poetry cache
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- install-deps.sh
+++ install-deps.sh
@@ -8,3 +8,4 @@ export DEBIAN_FRONTEND=noninteractive
 pip3 install --no-cache-dir poetry
 poetry config virtualenvs.create false
 poetry install --no-dev --no-interaction --no-ansi
+echo yes | poetry cache clear --all pypi
